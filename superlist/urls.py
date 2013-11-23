539673408949669f5df6d2f6799734927f61e91c
Zakatell Nur M. Kanda <zakatellkanda@gmail.com>
2013-11-23 16:01:14 +0800

    First unit test and url mapping, dummy view

--- superlist/urls.py
+++ superlist/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'superlist.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
