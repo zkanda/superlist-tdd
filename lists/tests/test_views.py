c85efedec255f2c93c4adb07380a1859248bf68f
Zakatell Nur M. Kanda <zakatellkanda@gmail.com>
2013-12-13 00:18:47 +0800

    use new form in home_page, simplify testse, NB breaks stuff!

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.utils.html import escape
 
+from lists.forms import ItemForm
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -15,13 +16,13 @@ class HomePageTest(TestCase):
 
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-        expected_html = render_to_string('home.html')
-
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
     def test_home_page_only_save_items_when_necessary(self):
         request = HttpRequest()
