02ddd39e38f360d39e5584050a8b9a1a68a23730
Zakatell Nur M. Kanda <zakatellkanda@gmail.com>
2013-12-12 21:08:06 +0800

    fix typo and wrong calling of functions

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -24,9 +24,9 @@ class ItemValidationTest(FunctionalTest):
         # She recieves a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empyt list item")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
-        self.browser.check_for_row_in_list_table('1: Buy Milk')
-        self.browser.check_for_row_in_list_table('1: Make tea')
+        self.check_for_row_in_list_table('1: Buy Milk')
+        self.check_for_row_in_list_table('1: Make tea')
